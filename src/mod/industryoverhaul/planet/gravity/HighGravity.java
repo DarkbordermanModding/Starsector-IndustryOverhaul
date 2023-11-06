7c12ce856ed11c8cf6ed2dd2cf19f8ff1fbd2762
Darkborderman <reastw1234@gmail.com>
2023-11-07 00:48:02 +0800

    Update

--- src/mod/industryoverhaul/planet/gravity/HighGravity.java
+++ src/mod/industryoverhaul/planet/gravity/HighGravity.java
@@ -6,7 +6,6 @@ import java.util.List;
 import com.fs.starfarer.api.impl.campaign.ids.Conditions;
 
 import mod.industryoverhaul.ConditionModifierIndustry;
-import mod.industryoverhaul.Utilities;
 
 public class HighGravity extends ConditionModifierIndustry{
     protected List<String> getPreventAnyConditions(){
@@ -18,10 +17,5 @@ public class HighGravity extends ConditionModifierIndustry{
 
     protected List<String> getAddedConditions(){
         return Arrays.asList(Conditions.HIGH_GRAVITY);
-    }
-
-    protected void buildingFinished() {
-        //Utilities.changePlanetSpec(market, "gas_giant");
-        super.buildingFinished();
     }
 }
--- src/mod/industryoverhaul/planet/gravity/RemoveGravity.java
+++ src/mod/industryoverhaul/planet/gravity/RemoveGravity.java
@@ -1,7 +1,37 @@
 package mod.industryoverhaul.planet.gravity;
 
+import java.util.Arrays;
+import java.util.List;
+
+import com.fs.starfarer.api.impl.campaign.ids.Conditions;
+
 import mod.industryoverhaul.ConditionModifierIndustry;
+import mod.industryoverhaul.Utilities;
 
 public class RemoveGravity extends ConditionModifierIndustry{
-    
+
+    protected List<String> getRequiredAnyConditions(){
+        return Arrays.asList(
+            Conditions.LOW_GRAVITY,
+            Conditions.HIGH_GRAVITY
+        );
+    }
+
+    protected List<String> getRemoveConditions(){
+        return Arrays.asList(
+            Conditions.LOW_GRAVITY,
+            Conditions.HIGH_GRAVITY
+        );
+    }
+
+    protected void buildingFinished() {
+        if(market.hasCondition(Conditions.IRRADIATED)){
+            Utilities.changePlanetSpec(market, "irradiated");
+        }else if(market.hasCondition(Conditions.TOXIC_ATMOSPHERE)){
+            Utilities.changePlanetSpec(market, "toxic");
+        }else {
+            Utilities.changePlanetSpec(market, "barren");
+        }
+        super.buildingFinished();
+    }
 }
